a828ff97488582b6bfd3273c0eb01ea2231d633c
Izem M <43759840+Izem0@users.noreply.github.com>
2023-02-26 00:19:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,16 +30,25 @@ streamlit.dataframe(fruits_to_show)
 # ========= Fruityvice api response ============= #
 streamlit.header('Fruityvice Fruit Advice!')
 
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information.')
+   else:
+    # get fruityvice data
+    fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # show data
+    streamlit.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  streamlit.error()
+ 
 # user input
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+
 streamlit.write('The user entered ', fruit_choice)
 
-# get fruityvice data
-fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-# show fruityvice data
-streamlit.dataframe(fruityvice_normalized)
 
 # ========= snowflake connector ============= #
 streamlit.stop()
